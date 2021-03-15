5c4a76ad304e511c948572159faff907cfa69b39
Patrick Gorton <ragaedude@gmail.com>
2021-03-15 21:14:23 +0100

    Working on the conversion matrix

--- misc.py
+++ misc.py
@@ -2,36 +2,35 @@ import numpy as np
 from numpy.matlib import repmat
 from matplotlib import pyplot as plt
 
-#  1  2  3  4
-#  5  6  7  8
-#  9 10 11 12
-# 13 14 15 16
+cal_points =    [(75, 100), (160, 1200), (1125, 95), (1173, 1280),
+                (160, 1250), (135, 2500), (1173, 1280), (1203, 2560),
+                (1125, 95), (1173, 1280), (2375, 76), (2399, 1199),
+                (1173, 1280), (1203, 2560), (2375, 1239), (2501, 2611)]
 
-cal_points =    [(100, 75), (1250, 160), (95, 1125), (1280, 1173),
-                (1250, 160), (2500, 135), (1280, 1173), (2560, 1203),
-                (95, 1125), (1280, 1173), (76, 2375), (1199, 2399),
-                (1280, 1173), (2560, 1203), (1239, 2375), (2611, 2501)]
-
-"""
-for i, p in enumerate(cal_points):
-    plt.plot(p[0], p[1], '*',label=f'{[i+1]}')
-    plt.legend(loc="upper left")
-plt.show()
-"""
+cal_points =    [(75, 100), (160, 1200), (160, 1250), (1125, 95), (1173, 1280),
+                (160, 1250), (135, 2500), (1173, 1280), (1203, 2560),
+                (1125, 95), (1173, 1280), (2375, 76), (2399, 1199),
+                (1173, 1280), (1203, 2560), (2375, 1239), (2501, 2611)]
 
 X = [100, 2500, 95, 2560]
-imh, imw = 6, 6
+imh, imw = 12, 12
 
-def fxy(x, y, boundaries, return_mat=False):
-    # TODO: This currently only works for one square made by calibration coordinates. Extend function
-    # so that it concatinates several squares (4?)
+def fxy(cal_points, imh, imw):
+
+    # TODO: Currently only works with 9 calibration points, or four quadrants.
+    # Generalise to as many quadrants as one would like
+
+    quadrants = [cal_points[0], cal_points[1], cal_points[3], cal_points[4],
+                 cal_points[1], cal_points[2], cal_points[4], cal_points[5],
+                 cal_points[3], cal_points[4], cal_points[6], cal_points[7],
+                 cal_points[4], cal_points[5], cal_points[7], cal_points[8]]
 
     conversion_mat_x = np.zeros((imh, imw))
     conversion_mat_y = np.zeros((imh, imw))
 
     # Upper left
-    upper_left_x = np.linspace((boundaries[0][0],boundaries[1][0]),(boundaries[2][0],boundaries[3][0]), imw//2)
-    upper_left_y = np.linspace((boundaries[0][1],boundaries[1][1]),(boundaries[2][1],boundaries[3][1]), imh//2)
+    upper_left_x = np.linspace((quadrants[0][0],quadrants[1][0]),(quadrants[2][0],quadrants[3][0]), imw//2)
+    upper_left_y = np.linspace((quadrants[0][1],quadrants[1][1]),(quadrants[2][1],quadrants[3][1]), imh//2)
     ulx = []
     for mat in upper_left_x:
         ulx.append(np.linspace(mat[0], mat[1], imw//2))
@@ -42,8 +41,8 @@ def fxy(x, y, boundaries, return_mat=False):
     conversion_mat_y[:imh//2, :imw//2] = np.array(uly).astype(int)
 
     # Upper right
-    upper_right_x = np.linspace((boundaries[4][0],boundaries[5][0]),(boundaries[6][0],boundaries[7][0]), imw//2)
-    upper_right_y = np.linspace((boundaries[4][1],boundaries[5][1]),(boundaries[6][1],boundaries[7][1]), imh//2)
+    upper_right_x = np.linspace((quadrants[4][0],quadrants[5][0]),(quadrants[6][0],quadrants[7][0]), imw//2)
+    upper_right_y = np.linspace((quadrants[4][1],quadrants[5][1]),(quadrants[6][1],quadrants[7][1]), imh//2)
     ulx = []
     for mat in upper_right_x:
         ulx.append(np.linspace(mat[0], mat[1], imw//2+1))
@@ -54,33 +53,32 @@ def fxy(x, y, boundaries, return_mat=False):
     conversion_mat_y[:imh//2, imw//2-1:imw] = np.array(uly).astype(int)
 
     # Lower left
-    lower_left_x = np.linspace((boundaries[8][0],boundaries[9][0]),(boundaries[10][0],boundaries[11][0]), imw//2)
-    lower_left_y = np.linspace((boundaries[8][1],boundaries[9][1]),(boundaries[10][1],boundaries[11][1]), imh//2)
+    lower_left_x = np.linspace((quadrants[8][0],quadrants[9][0]),(quadrants[10][0],quadrants[11][0]), imw//2+1)
+    lower_left_y = np.linspace((quadrants[8][1],quadrants[9][1]),(quadrants[10][1],quadrants[11][1]), imh//2+1)
     llx = []
     for mat in lower_left_x:
         llx.append(np.linspace(mat[0], mat[1], imw//2))
-    conversion_mat_x[imh//2:imh, :imw//2] = np.array(llx).astype(int)
+    conversion_mat_x[(imh-1)//2:imh, :imw//2] = np.array(llx).astype(int)
     lly = []
     for mat in lower_left_y:
         lly.append(np.linspace(mat[0], mat[1], imh//2))
-    conversion_mat_y[imh//2:imh, :imw//2] = np.array(lly).astype(int)
+    conversion_mat_y[(imh-1)//2:imh, :imw//2] = np.array(lly).astype(int)
 
     # Lower right
-    lower_right_x = np.linspace((boundaries[12][0],boundaries[13][0]),(boundaries[14][0],boundaries[15][0]), imw//2)
-    lower_right_y = np.linspace((boundaries[12][1],boundaries[13][1]),(boundaries[14][1],boundaries[15][1]), imh//2)
+    lower_right_x = np.linspace((quadrants[12][0],quadrants[13][0]),(quadrants[14][0],quadrants[15][0]), imw//2+1)
+    lower_right_y = np.linspace((quadrants[12][1],quadrants[13][1]),(quadrants[14][1],quadrants[15][1]), imh//2+1)
     lrx = []
     for mat in lower_right_x:
         lrx.append(np.linspace(mat[0], mat[1], imw//2+1))
-    conversion_mat_x[imh//2:imh, imw//2-1:imw] = np.array(lrx).astype(int)
+    conversion_mat_x[(imh-1)//2:imh, imw//2-1:imw] = np.array(lrx).astype(int)
     lry = []
     for mat in lower_right_y:
         lry.append(np.linspace(mat[0], mat[1], imh//2+1))
-    conversion_mat_y[imh//2:imh, imw//2-1:imw] = np.array(lry).astype(int)
-
+    conversion_mat_y[(imh-1)//2:imh, imw//2-1:imw] = np.array(lry).astype(int)
 
     return np.stack([conversion_mat_x, conversion_mat_y])
 
 
 mat = fxy(1, 1, cal_points)
-#print(mat[:,5,5])
-print(mat[:])
+print(mat[:,7:,7:])
+#print(mat[:])
--- servotest.py
+++ servotest.py
@@ -1,5 +1,6 @@
 from pynput.mouse import Listener as MouseListener
 from pynput.mouse import Button
+from misc import fxy
 import cv2
 from threading import Thread
 import pigpio
@@ -14,7 +15,7 @@ class VideoStream:
         ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
         ret = self.stream.set(3,resolution[0])
         ret = self.stream.set(4,resolution[1])
-            
+
         # Read first frame from the stream
         (self.grabbed, self.frame) = self.stream.read()
 
@@ -62,16 +63,16 @@ class PanTilt(object):
                  servo_min=500,
                  servo_max=2500,
                  pwm_frequency=50,
-                 image_width=500,
                  image_height=500,
+                 image_width=500,
                  ):
-        
+
         self.servo_min = servo_min
         self.servo_max = servo_max
-        
+
         self.pump_pin = pump_pin
         self.pump = pigpio.pi()
-        
+
         self.pan_pin = pan_pin
         self.tilt_pin = tilt_pin
         self.pan = pigpio.pi()
@@ -80,59 +81,66 @@ class PanTilt(object):
         self.tilt.set_mode(self.tilt_pin, pigpio.OUTPUT)
         self.pan.set_PWM_frequency(self.pan_pin, pwm_frequency)
         self.tilt.set_PWM_frequency(self.tilt_pin, pwm_frequency)
-        
+
         self.mouse_x_max = mouse_x_max
         self.mouse_y_max = mouse_y_max
-        
-        self.image_width = image_width
-        self.image_height = image_height
-        
+
+        self.imw = image_width
+        self.imh = image_height
+
         # Initialise pan and tilt positions to center
         self.pan_position = (servo_max - servo_min) / 2
         self.tilt_position = (servo_max - servo_min) / 2
-        
+
         self.calibrated = True
         self.video_stream = VideoStream(resolution=(500, 500),framerate=10)
         self.mouse_listener = MouseListener(on_move=self.on_move, on_click=self.on_click)
-        
-        self.calibration_points = [[(self.image_width*0.1, self.image_height*0.1), ()], # upper left
-                                   [(self.image_width-self.image_width*0.1, self.image_height*0.1), ()], # upper right
-                                   [(self.image_width*0.1, self.image_height-self.image_height*0.1), ()], # lower left
-                                   [(self.image_width-self.image_width*0.1, self.image_height-self.image_height*0.1), ()], # lower right
-                                   [(self.image_width//2, self.image_height//2), ()]] # center
-    
-    def image_to_servo_coordinates(x, y):
-        pass
-        
+
+        self.calibration_markers = [(0, 0), (0, self.imw//2), (0, self.imw),
+                                    (self.imh//2, 0), (self.imh//2, self.imw//2), (self.imh//2, self:imw),
+                                    (self.imh, self.imw//2), (self.imh, self.imw//2), (self.imh, self.imw)]
+
+        self.calibration_points =  [(), (), (),
+                                    (), (), ()
+                                    (), (), ()]
+
+        self.conversion_table = None
+
+    def aim_at(y, x):
+        self.tilt_position = y
+        self.pan_position = x
+        self.tilt.set_servo_pulsewidth(self.tilt_pin, self.tilt_position)
+        self.pan.set_servo_pulsewidth(self.pan_pin, self.pan_position)
+
     def calibrate(self):
         self.calibrated = False
-        
+
         self.video_stream.start()
         self.mouse_listener.start()
-        
+
         self.cal_point_idx = 0
         while not self.calibrated:
 
             if cv2.waitKey(1) == ord('q'):
                 break
-                
+
             # Grab frame from video stream
             frame = self.video_stream.read()
             frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
-            cal_point_coordinates = (int(self.calibration_points[self.cal_point_idx][0][0]),
-                                     int(self.calibration_points[self.cal_point_idx][0][1]))
+            cal_point_coordinates = (int(self.calibration_markers[self.cal_point_idx][0]),
+                                     int(self.calibration_markers[self.cal_point_idx][1]))
             cv2.circle(frame, cal_point_coordinates, 15, (0, 0, 255), 2)
             cv2.imshow('Object detector', frame)
-            
-                        
+
         print('\nCalibration finished!\n')
         self.mouse_listener.stop()
         self.video_stream.stop()
-        
+
         print('The following points are calibrated:')
         for cal_point in self.calibration_points:
             print(cal_point)
-        
+
+        self.conversion_matrix = fxy(self.calibration_points, self.imh, self.imw)
 
     def on_move(self, x, y):
         self.tilt_position = int(self.servo_min + ((y)/self.mouse_y_max) * (self.servo_max-self.servo_min))
@@ -145,7 +153,7 @@ class PanTilt(object):
             print('Mouse clicked at ({0}, {1}) with {2}'.format(x, y, button))
             if button == Button.left:
                 self.pump.write(self.pump_pin, 1)
-                
+
             elif button == Button.right:
                 self.calibrated = True
 
@@ -153,14 +161,14 @@ class PanTilt(object):
             if button == Button.left:
                 print(f'Saving calibration idx [{self.cal_point_idx}] to ({self.pan_position, self.tilt_position})')
                 self.pump.write(self.pump_pin, 0)
-                self.calibration_points[self.cal_point_idx][1] = (self.pan_position, self.tilt_position)
-                
+
+                self.calibration_points[self.cal_point_idx][0] = self.tilt_position
+                self.calibration_points[self.cal_point_idx][1] = self.pan_position
+
                 if self.cal_point_idx < len(self.calibration_points)-1:
                     self.cal_point_idx += 1
                 else:
                     self.calibrated = True
-            
-
 
 
 servo_tilt_pin = 13
@@ -170,10 +178,9 @@ pump_pin = 16
 try:
     pan_tilt = PanTilt(servo_pan_pin, servo_tilt_pin, pump_pin)
     pan_tilt.calibrate()
-    
+
 except KeyboardInterrupt:
     servo_y.set_PWM_dutycycle(servo_y_pin, 0)
     servo_y.set_PWM_frequency(servo_y_pin, 0)
     servo_x.set_PWM_dutycycle(servo_x_pin, 0)
     servo_x.set_PWM_frequency(servo_x_pin, 0)
-    
